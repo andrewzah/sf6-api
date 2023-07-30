5ff0e50c1d56119f591486431370de0dafa36dd0
ZahBenjamin <bz@benzah.com>
2023-07-30 13:56:08 -0400

    update sidekiq/scrape/rankings

--- cornbread_api/app/sidekiq/scrape/rankings_job.rb
+++ cornbread_api/app/sidekiq/scrape/rankings_job.rb
@@ -1,7 +1,21 @@
+require 'mongo'
+require 'json'
+
 class Scrape::RankingsJob
   include Sidekiq::Job
+  sidekiq_options :queue => :default , :retry => 1
 
-  def perform(*args)
-    # Do something
+
+  def perform(data)
+    # Connect to MongoDB server
+    client = Mongo.Client.new('mongodb://localhost:27017/cornbread')
+
+    # Get collection
+    collection = client[:cornbreadCollection]
+
+    # Convert JSON string to Ruby hash
+    json_data = JSON.parse(data)
+
+    response = collection.insert_one(json_data)
   end
-end
+  
