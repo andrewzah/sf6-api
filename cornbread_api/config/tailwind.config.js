4b578b711096aaf9cc9c8acac6ce7696b947312d
ZahBenjamin <bz@benzah.com>
2023-07-31 15:30:00 -0400

    Ranking partials, css

--- cornbread_api/config/tailwind.config.js
+++ cornbread_api/config/tailwind.config.js
@@ -8,6 +8,21 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
